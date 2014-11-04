3878c7fcda72a8a102d621d62a47a5ce3f8caab3
wangjia-s <wangjia-s@360.cn>
2014-11-04 10:11:03 +0800

    just commit

--- testAndroid/Demo1/src/com/example/Demo1/MainActivity.java
+++ testAndroid/Demo1/src/com/example/Demo1/MainActivity.java
@@ -1,9 +1,30 @@
 package com.example.Demo1;
 
 import android.app.Activity;
+import android.app.ActivityManager;
+import android.app.Application;
+import android.content.Context;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.graphics.drawable.Drawable;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Debug;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListAdapter;
+import android.widget.SimpleAdapter;
 
-public class MainActivity extends Activity {
+import java.util.*;
+
+public class MainActivity extends Activity implements Button.OnClickListener {
+
+    private final static String TAG = "MainActivity";
+
+    protected int[] btn_res_ids = {R.id.process_list_btn };
+
     /**
      * Called when the activity is first created.
      */
@@ -11,5 +32,60 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        initViews();
     }
+
+    protected void initViews() {
+        for (int id : btn_res_ids) {
+            findViewById(id).setOnClickListener(this);
+        }
+
+        List<Map<String, Object>> list = new ArrayList<Map<String, Object>>();
+
+        List<AppInfo> appinfos = loadPackage();
+        String[] from = new String[]{};
+        int[] to = new int[]{};
+        ListAdapter adapter = new SimpleAdapter(getApplicationContext(), list, 0, from, to);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+    }
+
+    public void showMsg(String msg) {
+        Log.i(TAG, msg);
+    }
+
+    public class AppInfo {
+        public String package_name;
+        public String app_name;
+        public int version_code;
+        public String version_name;
+        public Drawable icon_image;
+    }
+
+    protected List<AppInfo> loadPackage() {
+
+        List<AppInfo> appInfos = new ArrayList<AppInfo>();
+
+        PackageManager manager = getPackageManager();
+        List<PackageInfo> packages = manager.getInstalledPackages(0);
+        for (PackageInfo info : packages) {
+            AppInfo appinfo = new AppInfo();
+            appinfo.app_name = info.applicationInfo.loadLabel(manager).toString();
+            appinfo.icon_image = info.applicationInfo.loadIcon(manager);
+            appinfo.package_name = info.packageName;
+            appinfo.version_code = info.versionCode;
+            appinfo.version_name = info.versionName;
+            appInfos.add(appinfo);
+
+
+            String fmt = String.format("package : %s, packageName %s" , info.packageName, info.applicationInfo.loadLabel(manager).toString());
+            showMsg(fmt);
+        }
+        return appInfos;
+    }
+
+
 }
---
+++ testAndroid/Demo1/src/com/example/Demo1/MyAdapter.java
@@ -0,0 +1,30 @@
+package com.example.Demo1;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+/**
+ * Created by wangjia-s on 14-9-9.
+ */
+public class MyAdapter extends BaseAdapter {
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        return null;
+    }
+}
